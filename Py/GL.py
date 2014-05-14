0c95c07b48616f77324b260a513ade09f9ba45fb
user <no.email>
2014-05-14 10:56:09 -0400

    tinkering

--- Py/GL.py
+++ Py/GL.py
@@ -6,14 +6,13 @@ from graphlab import recommender, SFrame, aws
 aws.set_credentials('AKIAIWD7RQTJXCCB72BA', 'E0/g0PtP7d9jkjrSyOKmQNpVKXm6FajpF34A55tN')
 
 def test_graphlab():
+    url='http://s3.amazonaws.com/GraphLab-Datasets/movie_ratings/training_data.csv'
     ''' test the graphlab install '''
-    data = SFrame('s3:///graphlab-sample-data/example.csv')
-    m = recommender.create(data, user='user_id', item='item_id')
-    recs = m.recommend(k=5, user_ids=['u1', 'u3', 'u15'])
-    print recs
+    data = SFrame('url')
 
-    m = recommender.matrix_factorization.create(data, user='user_id', item='item_id',
+    m1 = recommender.matrix_factorization.create(data, user='user', item='movie',
             D=7, regularizer=0.05, nmf=True,use_bias=False)
                                         
-    m = recommender.item_similarity.create(data, user='user_id', item='item_id',
+    m2 = recommender.item_similarity.create(data, user='user', item='item',
                                                similarity_type='jaccard')        
+                                               
