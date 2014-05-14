2134b2ae070d25d3a3599e08a264dae456a825a1
user <no.email>
2014-05-14 12:15:12 -0400

    fixed url

--- Py/GL.py
+++ Py/GL.py
@@ -8,7 +8,7 @@ aws.set_credentials('AKIAIWD7RQTJXCCB72BA', 'E0/g0PtP7d9jkjrSyOKmQNpVKXm6FajpF34
 def test_graphlab():
     url='http://s3.amazonaws.com/GraphLab-Datasets/movie_ratings/training_data.csv'
     ''' test the graphlab install '''
-    data = SFrame('url')
+    data = SFrame(url)
 
     m1 = recommender.matrix_factorization.create(data, user='user', item='movie',
             D=7, regularizer=0.05, nmf=True,use_bias=False)
