47b35d406fcb9be8fef0df8109d0e4cffc40eba4
user <no.email>
2014-05-14 10:35:35 -0400

    added GL.py, graphlab

---
+++ Py/GL.py
@@ -0,0 +1,17 @@
+#graphlab
+
+#from readers import *
+from graphlab import recommender, SFrame
+
+def test_graphlab():
+    ''' test the graphlab install '''
+    data = SFrame('s3:///graphlab-sample-data/example.csv')
+    m = recommender.create(data, user='user_id', item='item_id')
+    recs = m.recommend(k=5, user_ids=['u1', 'u3', 'u15'])
+    print recs
+
+    m = recommender.matrix_factorization.create(data, user='user_id', item='item_id',
+            D=7, regularizer=0.05, nmf=True,use_bias=False)
+                                        
+    m = recommender.item_similarity.create(data, user='user_id', item='item_id',
+                                               similarity_type='jaccard')        
