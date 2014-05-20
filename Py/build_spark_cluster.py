b376a0abc1f0eba098fce8cf454530780426de25
David Johnston <dajohnst@thoughtworks.com>
2014-05-20 20:08:23 +0000

    ec2 command works now with start and stop, could use some twiddling

--- Py/build_spark_cluster.py
+++ Py/build_spark_cluster.py
@@ -1,11 +1,27 @@
 #build a spark cluster
+#needs some refactoring but works fine for now
 import json
 
-def build_spark_cmd(spark_file="../myspark.json"):
+def get_pars(spark_file="../myspark.json"):
     JSONDC=json.JSONDecoder()
     p=JSONDC.decode(open(spark_file,'rU').read())
-    cmd="spark-ec2 -k %s -i %s.pem -t %s"%(p['pem'],p['pem'],p['type'])
-    cmd=cmd+" -u %s -s %s -r %s"%(p['user'],p['n_slaves'],p['region'])
+    return p
+
+def stop_spark(p):
+    print 'stopping'
+    cmd="spark-ec2 -k %s -i %s.pem -r %s stop %s"%(p['pem'],p['pem'],p['region'],p['name'])
+    cmd=p['ec2_dir']+'/'+cmd
+    run_in_shell(cmd)
+
+def start_spark(p):
+    print 'starting'
+    cmd="spark-ec2 -k %s -i %s.pem -r %s start %s"%(p['pem'],p['pem'],p['region'],p['name'])
+    cmd=p['ec2_dir']+'/'+cmd
+    run_in_shell(cmd)
+
+def launch_spark(p):
+    cmd="spark-ec2 -k %s -i %s.pem -r %s"%(p['pem'],p['pem'],p['region'])
+    cmd=cmd+" -u %s -s %s -t %s"%(p['user'],p['n_slaves'],p['type'])
     cmd=cmd+" --ebs-vol-size %s"%p['diskGB']
     print p['spot_price'].__class__
     if p['spot_price'].__class__ in [int,float]:
@@ -23,7 +39,7 @@ def build_spark_cmd(spark_file="../myspark.json"):
     cmd=p['ec2_dir']+'/'+cmd
     print "command is:"
     print cmd
-    return cmd
+    run_in_shell(cmd)
 
 def run_in_shell(cmd):
     import os
@@ -31,11 +47,19 @@ def run_in_shell(cmd):
     os.system(full_cmd)
 
 if __name__ == "__main__":
+    #could use some cleaning up
     import sys
     args=sys.argv
-    if len(args) <= 1:
-        cmd=build_spark_cmd()
-    else :
-        spark_file=sys.argv[1] 
-        cmd=build_spark_cmd(spark_file)
-    run_in_shell(cmd)
+    if len(args) == 1:
+        p=get_pars()
+        launch_spark_pars(p)
+    elif len(args) == 2:
+        spark_file=sys.args[1] 
+        p=get_pars(spark_file)
+        launch_spark(p)
+    elif len(args) == 3:
+        if args[2] not in ['stop','start']: raise(ValueError,"I only know start and stop for second arg")
+        spark_file=sys.argv[1]
+        p=get_pars(spark_file)
+        if args[2] == 'start': start_spark(p)
+        if args[2] == 'stop': stop_spark(p)
