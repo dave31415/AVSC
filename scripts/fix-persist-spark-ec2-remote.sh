0ef2010fc7a5735de0437692a2f208c0190dffc2
David Johnston <dajohnst@thoughtworks.com>
2014-05-21 22:14:34 +0000

    fixed script permissions

--- scripts/fix-persist-spark-ec2-remote.sh
+++ scripts/fix-persist-spark-ec2-remote.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+#launched from the master
 SLAVES=`cat /root/spark-ec2/slaves`
-FIX_SCRIPT=./fix-persist-spark-ec2.sh
+FIX_SCRIPT=fix-persist-spark-ec2.sh
+#fix yourself before helping others
+/root/$FIX_SCRIPT
+
 for slave in $SLAVES; do
     echo "fixing slave $slave"
     chmod u+x $FIX_SCRIPT
-    scp $FIX_SCRIPT $slave:
-    ssh $slave $FIX_SCRIPT
-    ssh $slave rm $FIX_SCRIPT
+    scp /root/$FIX_SCRIPT $slave:
+    ssh $slave /root/$FIX_SCRIPT
+    ssh $slave rm /root/$FIX_SCRIPT
 done
